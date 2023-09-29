9bc4a4e52665c8177900c49646daaa56b1ea1e1f
MollikaComputer Provider <mollikacomputer3@gmail.com>
2023-09-29 15:48:30 +0600

    dark light theme added

--- src/app/(home)/HomePage.js
+++ src/app/(home)/HomePage.js
@@ -2,13 +2,11 @@ import React from 'react';
 import HeroSection from './HeroSection';
 import Comments from './Comments';
 import CountUpPage from './CountUpPage';
-import HomePageServices from './HomePageServices';
 
 const HomePage = async() => {
     return (
         <div>
             <HeroSection/>
-            <HomePageServices/>
             <Comments/>
             <h2 className='text-center text-3xl font-bold pb-12' >Our Company Status</h2>
             <CountUpPage/>
