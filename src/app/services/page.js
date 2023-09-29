62a0053cd2e6c264ec23027a446475e2198c109c
MollikaComputer Provider <mollikacomputer3@gmail.com>
2023-09-29 15:55:14 +0600

    data file change

--- src/app/services/page.js
+++ src/app/services/page.js
@@ -1,4 +1,3 @@
-import getServices from "@/utils/getServices";
 import SingleService from "./SingleService";
 
 export const metadata={
@@ -6,7 +5,29 @@ export const metadata={
     description:"carpet cleaning service description",
 };
 const ServicePage = async() => {
-    const services = await getServices();
+    const services=[
+        {
+            "id":1,
+            "title":"Carpet cleaning Service",
+            "body":"Carpet cleaning service details here...",
+            "imageUrl":"/cleaning.jpg"
+        },
+        {
+            "id":2,
+            "title":"Deep Cleaning",
+            "body":"Deep cleaning service details here... ",
+            "price":550,
+            "imageUrl":"/deep_cleaning.jpg"
+        },
+        {
+            "id":3,
+            "title":"Rug Cleaning",
+            "body":"Rug cleaning service details here... ",
+            "price":600,
+            "imageUrl":"/rug_cleaning.jpg"
+        }
+    ]
+
     return (
         <div className="container mx-auto my-16">
            <h2 className='text-2xl text-green-500 font-semibold text-center my-10'>Service Page</h2>
